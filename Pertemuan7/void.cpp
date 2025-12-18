97b1c2183e493ca9f4adc442a037e8b2e6c43ed6
maeyuma <thefastman64@gmail.com>
2025-12-18 23:20:46 +0700

    Create void.cpp

---
+++ Pertemuan7/void.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void perkenalan(){
+    cout << "Hi, nama saya adalah Isma Eru Salsabila" << endl;
+    cout << "Saya adalah pacar Karina aespa" << endl;
+    cout << "Wuff you aespa sayang" << endl;
+}
+
+int main(){
+    perkenalan();
+    return 0;
+}
